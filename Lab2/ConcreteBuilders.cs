28698cfce75ac8150046f45435d4497bc1eeaa72
KlichnikovDA <spacerus99@gmail.com>
2020-02-06 17:59:19 +0500

    Added more ConcreteBuilders.

--- Lab2/ConcreteBuilders.cs
+++ Lab2/ConcreteBuilders.cs
@@ -48,4 +48,81 @@ namespace Lab2
             return this.House;
         }
     }
+
+    class SmartHouseBuilder2 : SmartHouseBuilder
+    {
+        public SmartHouseBuilder2()
+        {
+            this.House = new SmartHouse();
+        }
+
+        public override void BuildSensors()
+        {
+            this.House.Sensors = new Sensor("Комплект сенсоров 2");
+        }
+
+        public override void BuildExecutives()
+        {
+            this.House.Executives = new Executive("Исполнительный механизм 2");
+        }
+
+        public override void BuildPanels()
+        {
+            this.House.Panels = new Panel("Навороченная панель оператора");
+        }
+
+        public override void BuildMultimedia()
+        { }
+
+        public override void BuildControllers()
+        {
+            this.House.Controllers = new Controller("Набор контроллеров 2");
+        }
+
+        public override void BuildNetworks()
+        { }
+
+        public override SmartHouse GetResult()
+        {
+            return this.House;
+        }
+    }
+    class SmartHouseBuilder3 : SmartHouseBuilder
+    {
+        public SmartHouseBuilder3()
+        {
+            this.House = new SmartHouse();
+        }
+
+        public override void BuildSensors()
+        {
+            this.House.Sensors = new Sensor("Комплект сенсоров 3");
+        }
+
+        public override void BuildExecutives()
+        {
+            this.House.Executives = new Executive("Исполнительный механизм 3");
+        }
+
+        public override void BuildPanels()
+        { }
+
+        public override void BuildMultimedia()
+        { }
+
+        public override void BuildControllers()
+        {
+            this.House.Controllers = new Controller("Набор контроллеров 3");
+        }
+
+        public override void BuildNetworks()
+        {
+            this.House.Networks = new Network("Многочисленные сетевые устройства");
+        }
+
+        public override SmartHouse GetResult()
+        {
+            return this.House;
+        }
+    }
 }
--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -16,10 +16,21 @@ namespace Lab2
             SmartHouseBuilder Builder1 = new SmartHouseBuilder1();
             // строим дом
             SmartHouse House1 = HouseDirector.Construct(Builder1);
-
             Console.WriteLine("1-й сконструированный умный дом:");
             Console.WriteLine(House1.ToString());
-            
+            // создаем строителя 2
+            SmartHouseBuilder Builder2 = new SmartHouseBuilder2();
+            // строим дом
+            SmartHouse House2 = HouseDirector.Construct(Builder2);
+            Console.WriteLine("2-й сконструированный умный дом:");
+            Console.WriteLine(House2.ToString());
+            // создаем строителя 3
+            Builder1 = new SmartHouseBuilder3();
+            // строим дом
+            House1 = HouseDirector.Construct(Builder1);
+            Console.WriteLine("3-й сконструированный умный дом:");
+            Console.WriteLine(House1.ToString());
+
             Console.ReadLine();
         }
     }
